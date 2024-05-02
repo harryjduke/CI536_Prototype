c339f4dd31261841f868cf74ea09b97e98bb4726
harryjduke <harryjduke@gmail.com>
2024-05-02 20:27:07 +0100

    Added crouching to APlayerCharacter - Added input action for crouching - Bound crouch and uncrouch input to crouching functionality - Added functionality to offset the view when crouching and linearly interpolate it back the so that the crouching motion appears smooth
    Signed-off-by: harryjduke <harryjduke@gmail.com>

--- Source/CI536_Prototype/PlayerCharacter.cpp
+++ Source/CI536_Prototype/PlayerCharacter.cpp
@@ -17,6 +17,9 @@ DEFINE_LOG_CATEGORY(LogPlayerCharacter);
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
+	// Set this character to call Tick() every frame. You can turn this off to improve performance if you don 't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
 	// Set size for collision capsule
 	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
 		
@@ -38,6 +41,10 @@ APlayerCharacter::APlayerCharacter()
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
 
+	// Enable crouching
+	//GetCharacterMovement()->GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetCharacterMovement()->NavAgentProps.bCanCrouch = true;
+
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
@@ -48,6 +55,10 @@ APlayerCharacter::APlayerCharacter()
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+	// Configure crouching
+	CrouchEyeOffset = FVector(0.f);
+	CrouchSpeed = 12.f;
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
@@ -69,6 +80,65 @@ void APlayerCharacter::BeginPlay()
 	}
 }
 
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Wind CrouchEyeOffset down to zero using linear interpolation
+	const float CrouchInterpTime = FMath::Min(1.f, CrouchSpeed * DeltaTime);
+	CrouchEyeOffset = (1.f - CrouchInterpTime) * CrouchEyeOffset;
+}
+
+// Set CrouchEyeOffset to initially negate the crouch effect (through CalcCamera)
+void APlayerCharacter::OnStartCrouch(const float HalfHeightAdjust, const float ScaledHalfHeightAdjust)
+{
+	if(HalfHeightAdjust == 0.f) // Filter if there is no change in the height from the crouch
+	{
+		return;
+	}
+
+	const float StartBaseEyeHeight = BaseEyeHeight;
+	Super::OnStartCrouch(HalfHeightAdjust, ScaledHalfHeightAdjust);
+	/* Calculate the difference in eye height (above the collision center) and add the change in height of the
+	 collision center to calculate how much the camera view would have to be offset by to negate change in camera
+	 position caused by the crouch */
+	CrouchEyeOffset.Z += StartBaseEyeHeight - BaseEyeHeight + HalfHeightAdjust;
+	if (FollowCamera)
+	{
+		FollowCamera->SetRelativeLocation(FVector(0.f, 0.f, BaseEyeHeight), false);
+	}
+}
+
+void APlayerCharacter::OnEndCrouch(const float HalfHeightAdjust, const float ScaledHalfHeightAdjust)
+{
+	if(HalfHeightAdjust == 0.f) // Check the character
+	{
+		return;
+	}
+
+	const float StartBaseEyeHeight = BaseEyeHeight;
+	Super::OnEndCrouch(HalfHeightAdjust, ScaledHalfHeightAdjust);
+	/* Calculate the difference in eye height (above the collision center) and add the change in height of the
+	 collision center to calculate how much the camera view would have to be offset by to negate change in camera
+	 position caused by the uncrouch */
+	CrouchEyeOffset.Z += StartBaseEyeHeight - BaseEyeHeight - HalfHeightAdjust;
+	if (FollowCamera)
+	{
+		FollowCamera->SetRelativeLocation(FVector(0.f, 0.f, BaseEyeHeight), false);
+	}
+}
+
+// Offset the camera view by the CrouchEyeOffset to control the crouching transition
+void APlayerCharacter::CalcCamera(const float DeltaTime, FMinimalViewInfo& OutResult)
+{
+	if (FollowCamera)
+	{
+		FollowCamera->GetCameraView(DeltaTime, OutResult);
+		OutResult.Location += CrouchEyeOffset;
+	}
+}
+
+
 //////////////////////////////////////////////////////////////////////////
 // Input
 
@@ -82,6 +152,10 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
 		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
+		// Crouching
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Started, this, &APlayerCharacter::StartCrouch);
+		EnhancedInputComponent->BindAction(CrouchAction, ETriggerEvent::Completed, this, &ACharacter::UnCrouch, false);
+
 		// Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APlayerCharacter::Move);
 
@@ -92,6 +166,12 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	{
 		UE_LOG(LogPlayerCharacter, Error, TEXT("'%s' Failed to find an Enhanced Input component! This template is built to use the Enhanced Input system. If you intend to use the legacy system, then you will need to update this C++ file."), *GetNameSafe(this));
 	}
+}
+
+void APlayerCharacter::StartCrouch()
+{
+	if (GetCharacterMovement()->IsMovingOnGround())
+		Crouch();
 }
 
 void APlayerCharacter::Move(const FInputActionValue& Value)
--- Source/CI536_Prototype/PlayerCharacter.h
+++ Source/CI536_Prototype/PlayerCharacter.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
@@ -26,6 +27,14 @@ class CI536_PROTOTYPE_API APlayerCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
+
+	/** How much the view is offset from the eye position */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Crouch, meta = (AllowPrivateAccess = "true"))
+	FVector CrouchEyeOffset;
+
+	/** How quickly CrouchEyeOffset will return to zero */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category=Crouch, meta = (AllowPrivateAccess = "true"))
+	float CrouchSpeed;
 	
 	/** MappingContext */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
@@ -35,6 +44,10 @@ class CI536_PROTOTYPE_API APlayerCharacter : public ACharacter
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* JumpAction;
 
+	/** Crouch Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* CrouchAction;
+
 	/** Move Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* MoveAction;
@@ -42,7 +55,13 @@ class CI536_PROTOTYPE_API APlayerCharacter : public ACharacter
 	/** Look Input Action */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
+	
+	/** Sprint Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* SprintAction;
 
+	
+	
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
@@ -53,18 +72,49 @@ protected:
 
 	/** Called for looking input */
 	void Look(const FInputActionValue& Value);
+
+	/** Called for starting sprinting input */
+	void StartSprinting ();
+
+	/** Called for stopping sprinting input*/
+	void StopSprinting();
 	
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	
+
+	/** Called to check if the character is grounded before crouching*/
+	void StartCrouch();
+
 	// To add mapping context
 	virtual void BeginPlay() override;
+
+	// To wind CrouchEyeOffset Down to zero
+	virtual void Tick(float DeltaTime) override;
 
 public:	
 	
 	/** Returns CameraBoom subobject **/
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	
 	/** Returns FollowCamera subobject **/
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	/**
+	 * Implementation of OnStartCrouch for PlayerCharacter.
+	 * Set CrouchEyeOffset to initially negate the crouch camera offset effect (through CalcCamera).
+	 */
+	virtual void OnStartCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
+
+	/**
+	 * Implementation of OnEndCrouch for PlayerCharacter.
+	 * Set CrouchEyeOffset to initially negate the crouch camera offset effect (through CalcCamera).
+	 */
+	virtual void OnEndCrouch(float HalfHeightAdjust, float ScaledHalfHeightAdjust) override;
+
+	/**
+	 * Implementation of CalcCamera for PlayerCharacter
+	 * Offset the camera view by CrouchEyeOffset
+	 */
+	virtual void CalcCamera(float DeltaTime, FMinimalViewInfo& OutResult) override;
 
 };
